df28a6441689995e1d36bafbe0b8b9037414dae3
rhenninger61 <126610414+rhenninger26@users.noreply.github.com>
2025-04-21 10:38:06 -0400

    LCD Displays Frequency correctly :)

--- RhythmRelay/RhythmRelay.ino
+++ RhythmRelay/RhythmRelay.ino
@@ -2,24 +2,33 @@
 #include <TEA5767Radio.h>
 #include <LiquidCrystal_I2C.h>
 
+// --------- PLAY/PAUSE BUTTON -----------
+int buttonPin = 2;
+
+// -------- FREQUENCY POTENTIOMETER ---------
 int frequencyPotPin = A0; // Potentiometer output connected to analog pin 0
 int previousFrequency = 0;
+
+// ------------- LCD ----------
 // LCD initialization to the library with the number of the interface pins
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+// -------- RADIO MODULE ----------
 // Radio module initialization
 TEA5767Radio radio = TEA5767Radio();
 
 void setup() {
   // Radio initialization
   Wire.begin();
-  radio.setFrequency(93.7); // pick your own frequency
   Serial.begin(9600);
 
   // LCD initalization
   lcd.init(); // initialize the lcd
   lcd.backlight(); // Turn on backlight
   lcd.print("  Rhythm Relay");// Print a message to the LCD
+
+  // Button initalization
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
@@ -43,10 +52,15 @@ void adjustRadioFrequency()
 
 
   // Adjust radio frequency
-  if(frequency - previousFrequency >= 0.1f || previousFrequency - frequency >= 0.1f)
+  if (abs(frequency - previousFrequency) >= 0.1f) // Simplified condition
   {
-    radio.setFrequency(frequency);
-    lcd.print(String(frequency, 1));
-    previousFrequency = frequency;    
+      radio.setFrequency(frequency);
+
+      // Clear the LCD before printing the new frequency
+      lcd.clear();
+      lcd.print(String(frequency, 1)); // Display frequency with 1 decimal place
+      previousFrequency = frequency;
   }
+
+
 }
